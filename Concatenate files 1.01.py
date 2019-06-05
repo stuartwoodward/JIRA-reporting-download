4bc5ff722ed9a27615b1fad0f0089039afd773db
stuartwoodward <44180073+stuartwoodward@users.noreply.github.com>
2019-06-06 08:35:40 +1000

    Upload version 1.01 to 1.02
    Export to excel instead of csv

--- Concatenate files 1.01.py
+++ Concatenate files 1.01.py
@@ -1,35 +1,50 @@
- #Use "pip install pandas" in Python from command prompt if you dont have pandas already installed
+#Use "pip install pandas" in Python from command prompt if you dont have pandas already installed
 #Use "pip install openpyxl" in Python from command prompt to install openpyx1 required to export to excel
 import pandas as pd
 import os
 import csv
 
-
-Column_Filter = []
 li = []
-HF = 'C:\\Users\\stuart.woodward\\Desktop\\06. Python\\Config\\HeaderFilter.csv'
-outputfile = 'C:\\Users\\stuart.woodward\\Desktop\\06. Python\\All projects Intelehub (Jira).xlsx'
-excel_sheet = 'All projects Intelehub (Jira)'
+outputfilepath = 'C:\\Users\\stuart.woodward\\Desktop\\06. Python\\'
+import_field = 'Fix versions'
+ImportIndex=[]
+KeyIndex=[]
+#TargetFields =['Fix versions','Labels','Custom field (Services Impacting Deployments)']
 
-
-#Get the columns to import from a csv file
-with open(HF, 'r') as f:
-  reader = csv.reader(f)
-  Column_Filter = list(reader)
+outputfile=outputfilepath+import_field+'.xlsx'
 
 #Get the names of all csv files in a directory
 all_csv = [file_name for file_name in os.listdir(os.getcwd()) if '.csv' in file_name]
 
-#Import the required columns from multiple csv files and append to 1 file
+#Import the data from multiple csv files to a panda data frame
 for filename in all_csv:
-    df = pd.read_csv(filename, index_col=None, header=0, parse_dates=True, infer_datetime_format=True,usecols=Column_Filter[0])
-    li.append(df)
-#print (li)
+    df = pd.read_csv(filename, index_col=None, header=0, parse_dates=True, infer_datetime_format=True)
 
-frame = pd.concat(li, axis=0, ignore_index=True)
-frame.to_excel (outputfile, sheet_name = excel_sheet,index = False, header=True)
-#frame.to_csv('All projects Intelehub (Jira).csv', index=False)
+    #find the columns that match the Issue key and Import field description
+    columns = list(df.columns.values)
+    for column in columns:
+        if column.startswith(import_field):
+           ImportIndex.append(columns.index(column))
+        if column.startswith("Issue key"):
+           KeyIndex.append(columns.index(column))
 
+    #convert each row of a frame to a tuple
+    for row in df.itertuples(index=False):
+         
+        #create multiple tuples where the import field is repeated
+        for column in ImportIndex:
+            #Remove rows which are numbers. Nulls are imported from csv as nan in dataframes. 
+            #this loop removes the rows that have nan as a value
+            if type(row[column]) == str:
+                modifiedrows = [str(row[KeyIndex[0]]) ,str(row[column])]
+                li.append(modifiedrows)
 
+    #clear the index for each file as the labels can be in different columns
+    ImportIndex.clear()
+    KeyIndex.clear()
 
+#convert the tuple to a panda data frame
+frame = pd.DataFrame(li, columns = ['Issue key',import_field])
 
+#convert the panda data frame to an excel file
+frame.to_excel (outputfile, sheet_name = import_field,index = False, header=True)
